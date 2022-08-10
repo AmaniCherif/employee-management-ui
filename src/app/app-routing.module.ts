51a46ba2f634f734a9a582287c6d5329d6528557
Amani Cherif <73389025+AmaniCherif@users.noreply.github.com>
2022-08-10 17:12:37 +0100

    Front End module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContratComponent } from './contrat/contrat.component';
+import { DepartmentComponent } from './department/department.component';
+import { EmployeComponent } from './employe/employe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'contrats', component: ContratComponent},
+  {path: 'employees', component: EmployeComponent},
+  {path: 'departments', component: DepartmentComponent},
+  {path: '', redirectTo: 'contrats', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContratComponent } from './contrat/contrat.component';
+import { EmployeComponent } from './employe/employe.component';
+import { DepartmentComponent } from './department/department.component';
+import { EntrepriseComponent } from './entreprise/entreprise.component';
+import { FormsModule } from '@angular/forms';
+import { EmployeService } from './employe.service';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContratComponent,
+    EmployeComponent,
+    DepartmentComponent,
+    EntrepriseComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
-  providers: [],
+  providers: [EmployeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
